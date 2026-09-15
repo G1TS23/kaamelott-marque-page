36bae989c56bd15794ef1c46424deb3b62e215e8
olivierfalahi <olivier.falahi@gmail.com>
2026-09-15 21:20:17 +0200

    "Augmente l'épaisseur du trait de la croix (stroke-width 3)"

--- site/src/components/RechercheMobile.svelte
+++ site/src/components/RechercheMobile.svelte
@@ -131,7 +131,7 @@
             <path
               d="M6 6l12 12M18 6L6 18"
               stroke="currentColor"
-              stroke-width="2.5"
+              stroke-width="3"
               stroke-linecap="round"
               fill="none"
             />
